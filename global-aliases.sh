cf647890309e5fcc8aeca1780d45585c131dd979
Dee'Kej <admin@deekej.io>
2023-01-30 01:13:02 +0100

    global-aliases: 'apl' updated to use vault-pass-prompt

--- global-aliases.sh
+++ global-aliases.sh
@@ -107,7 +107,7 @@ case "$(uname -n)" in
 
     # Additional Ansible aliases:
     # ---------------------------
-    alias apl='ansible-playbook --vault-pass-file=/usr/local/bin/vault-pass-prompt'
+    alias apl='ansible-playbook --vault-password-file=/usr/local/bin/vault-pass-prompt'
     alias ari='ansible-role-init'
     alias afi='ansible_file_init'
     alias ali='ansible_layout_init'
